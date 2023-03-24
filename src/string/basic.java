3cc9d2c96242b41c50994638041f7c6f8c84d6b6
radharaman7262 <119476390+radharaman7262@users.noreply.github.com>
2023-03-24 16:56:18 +0530

    second commit

--- src/string/basic.java
+++ src/string/basic.java
@@ -1,6 +1,6 @@
 public class basic{
     public static void main(String args[]){
-        String args=("raman");
-        System.out.println(args);
+        String arg=("raman");
+        System.out.println(arg);
     }
 }
---
+++ src/string/firsttry.java
@@ -0,0 +1,7 @@
+package string;
+
+public class firsttry {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
